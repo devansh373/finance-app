403379ad0e5db883a7c0d3c7ee99f38564472ffb
Devansh Raghav <Devanshraghavsn@gmail.com>
2025-09-28 19:22:05 +0530

    changed deployed url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://finance-app-backend-z8v0.onrender.com/", 
+  baseURL: "https://finance-app-backend-z8v0.onrender.com/api", 
   // baseURL: "http://localhost:5000/api", 
   withCredentials: true, 
 });
