5e489a4257933d63b567d2d30faab12d8220ab5e
Devansh Raghav <Devanshraghavsn@gmail.com>
2025-09-30 19:45:22 +0530

    code cleaning

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "/api", 
-  // baseURL: "https://finance-app-backend-z8v0.onrender.com/api", 
-  // baseURL: "http://localhost:5000/api", 
+   
   baseURL: process.env.NEXT_PUBLIC_API_URL, 
   withCredentials: true, 
 });
